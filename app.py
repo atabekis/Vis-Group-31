046d35fd9abdd5bbf27cab6b9dbe143e151fd59d
Bartan <bartanoren@hotmail.com>
2022-11-25 11:48:00 +0100

    App update with map

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from jbi100_app.views.menu import make_menu_layout
 from jbi100_app.views.scatterplot import Scatterplot
 
 from dash import html
-import plotly.express as px
+from dash import dcc
 from dash.dependencies import Input, Output
 
+import plotly.express as px
+
 import pandas as pd
+import numpy as np
+
 
 
 if __name__ == '__main__':
@@ -15,9 +19,24 @@ if __name__ == '__main__':
 
     df = px.data.iris()  #TODO: remove and replace with preprocessed data (Ata)
 
-    # Instantiate custom views TODO: replace
-    scatterplot1 = Scatterplot("Scatterplot 1", 'sepal_length', 'sepal_width', df)
-    scatterplot2 = Scatterplot("Scatterplot 2", 'petal_length', 'petal_width', df)
+    # Dictionary of important locations in New York TODO; avoid plagiarism ;)
+    list_of_locations = {
+        "Madison Square Garden": {"lat": 40.7505, "lon": -73.9934},
+        "Yankee Stadium": {"lat": 40.8296, "lon": -73.9262},
+        "Empire State Building": {"lat": 40.7484, "lon": -73.9857},
+        "New York Stock Exchange": {"lat": 40.7069, "lon": -74.0113},
+        "JFK Airport": {"lat": 40.644987, "lon": -73.785607},
+        "Grand Central Station": {"lat": 40.7527, "lon": -73.9772},
+        "Times Square": {"lat": 40.7589, "lon": -73.9851},
+        "Columbia University": {"lat": 40.8075, "lon": -73.9626},
+        "United Nations HQ": {"lat": 40.7489, "lon": -73.9680},
+    }
+
+    fig = px.scatter_mapbox(open_ABNB_data, lat="lat", lon="long", hover_name="NAME", hover_data=["room type", "price"],
+                        color_discrete_sequence=["fuchsia"], zoom=10, height=500)
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r":0,"t":0,"l":10,"b":50})
+    # fig.show()
 
     app.layout = html.Div(
         id="app-container",
@@ -34,29 +53,14 @@ if __name__ == '__main__':
                 id="right-column",
                 className="nine columns",
                 children=[
-                    scatterplot1,
-                    scatterplot2
+                    dcc.Graph(
+                        id='map',
+                        figure=fig
+                    )
                 ],
             ),
         ],
     )
-
-    # Define interactions
-    @app.callback(
-        Output(scatterplot1.html_id, "figure"), [
-        Input("select-color-scatter-1", "value"),
-        Input(scatterplot2.html_id, 'selectedData')
-    ])
-    def update_scatter_1(selected_color, selected_data):
-        return scatterplot1.update(selected_color, selected_data)
-
-    @app.callback(
-        Output(scatterplot2.html_id, "figure"), [
-        Input("select-color-scatter-2", "value"),
-        Input(scatterplot1.html_id, 'selectedData')
-    ])
-    def update_scatter_2(selected_color, selected_data):
-        return scatterplot2.update(selected_color, selected_data)
 
 
     app.run_server(debug=False, dev_tools_ui=False)
