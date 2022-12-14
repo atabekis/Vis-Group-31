daafc5c960257dec0bcf88758c30a7e20283d3ef
atabekis <a.bekishoglu@student.tue.nl>
2022-12-14 14:35:33 +0300

    created the get_neighbourhood function that returns the neighbourhood groups as keys and its unique neighbourhoods as items. function completes in approximately 0.6 seconds

--- jbi100_app/config.py
+++ jbi100_app/config.py
@@ -1,28 +1,46 @@
 import pandas as pd
+import csv
+
 # Here you can add any global configurations
 
 color_list1 = ["green", "blue"]
 color_list2 = ["red", "purple"]
 neighbourhood_group = []
 
-data = pd.read_csv("Data/airbnb_open_data_clean.csv")
+data = pd.read_csv("../Data/airbnb_open_data_clean.csv")
 
-#returns the neighbourhood groups in the data
+
+# returns the neighbourhood groups in the data
 def get_neighbourhood_groups():
     neighbourhood_group = data['neighbourhood_group'].unique()
     return neighbourhood_group
 
-#returns the maximum and the minimum price in the data
+
+def get_neighbourhood():
+    neighbourhoods = {'Manhattan': [], 'Staten Island': [], 'Brooklyn': [], 'Bronx': [], 'Queens': []}
+
+    with open("../Data/airbnb_open_data_clean.csv", encoding='utf-8') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for line in reader:
+            neighbourhood = line[7]
+            group = line[6]
+
+            if (group in neighbourhoods.keys()) and (neighbourhood not in neighbourhoods[group]):
+                if neighbourhood != '0':
+                    neighbourhoods[group].append(neighbourhood)
+
+    return neighbourhoods
+
+
+# returns the maximum and the minimum price in the data
 def get_price_min_max():
     max = data['price'].max()
     min = data['price'].min()
     return int(min), int(max)
 
-#returns the possible values of instant bookability
+
+# returns the possible values of instant bookability
 def get_inst_bookable():
     bookable = data['instant_bookable'].unique()
     return bookable
-
-
-
-
