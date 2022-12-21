1d16e40ee72728b658dd141cdc4e5d735273cee4
atabekis <a.bekishoglu@student.tue.nl>
2022-12-21 17:56:22 +0300

    created the get_neighbourhood function that returns the neighbourhood groups as keys and its unique neighbourhoods as items. function completes in approximately 0.6 seconds + Added explanation

--- jbi100_app/config.py
+++ jbi100_app/config.py
@@ -7,7 +7,7 @@ color_list1 = ["green", "blue"]
 color_list2 = ["red", "purple"]
 neighbourhood_group = []
 
-data = pd.read_csv("../Data/airbnb_open_data_clean.csv")
+data = pd.read_csv("Data/airbnb_open_data_clean.csv")
 
 
 # returns the neighbourhood groups in the data
@@ -15,9 +15,11 @@ def get_neighbourhood_groups():
     neighbourhood_group = data['neighbourhood_group'].unique()
     return neighbourhood_group
 
+
 # Returns the unique neighbourhoods as items and their groups as keys in a dictionary
 def get_neighbourhood():
-    neighbourhoods = {'Manhattan': [], 'Staten Island': [], 'Brooklyn': [], 'Bronx': [], 'Queens': []}
+    neighbourhoods = {'All': list(neighbourhood_group), 'Manhattan': [], 'Staten Island': [], 'Brooklyn': [],
+                      'Bronx': [], 'Queens': []}
 
     with open("../Data/airbnb_open_data_clean.csv", encoding='utf-8') as f:
         reader = csv.reader(f)
