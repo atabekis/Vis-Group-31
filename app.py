f189aa1cb4613b5126682a2c989e737b4cc58e36
atabekis <a.bekishoglu@student.tue.nl>
2023-01-18 14:08:53 +0100

    PLEASE CHECK THE TODOs AT THE BEGINNING OF APP.PY

--- app.py
+++ app.py
@@ -1,22 +1,49 @@
 import dash
-# import dash_loading_spinners
-from dash import Dash, dcc, html
-from dash.dependencies import Input, Output, State
-from dash.exceptions import PreventUpdate
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import numpy as np
+import pandas as pd
+from textwrap import dedent
+# --------- Importing Base Functions ------------#
 import functions.config as config
 from functions.mapboxplot import Mapboxplot
 from functions.choropleth_mapbox import Choropleth
 from functions.barchart import Barchart
-import numpy as np
-import pandas as pd
-from textwrap import dedent
-# import dash_loading_spinners
-import time
+
+"""
+JBI100 - Visualizations Final Project
+Group No: 31
+Bartan Ören, Luc Tortike, Ata Bekişoğlu, Sarp Akar
+--------------------------------------------------------------------------------------------------------------
+This app.py file is seperated into multiple building functions for the construction of the Dash web application.
+
+->The html part of the code can be found in the builder functions such as 'build_banner()' and others. These are then 
+  combined within each other and in the app.layout line. 
+  
+->The styling (CSS) part of the web application can be found in the 'assets' directory. The dash library automatically
+  includes the styling given className as a parameter.
+"""
+
+# ------------------------------------------------------------------------------------------------------#
+# ------------------------------- main todos go here please ------------------------------------------- #
+# *: Not important, **: Important, ***: Very important
+
+# TODO: **Word/text processing should be graphed -> Bartan, Ata
+# TODO: *Ata: To find a way to load in the graphs faster when pc is not plugged in lmao
+# TODO: *Select better color palette for application
+# TODO: **Find a better way to graph the barchart + change color palette to match with the choropleth
+# ------------------------------------------------------------------------------------------------------#
+
 
 app = dash.Dash(__name__)
 
 
 def build_banner():
+    """
+    Builds the banner on the top of the application with app name, group number, TU/e logo and a button for explanation.
+    :return: dash.html object
+    """
+
     return html.Div(
         id='banner',
         className='banner',
@@ -34,6 +61,10 @@ def build_banner():
 
 
 def build_explanation():
+    """
+    Builds the markdown that pops up when the "What's this" button is pressed.
+    :return: dash.html object
+    """
     return html.Div(
         id='markdown',
         className='modal',
@@ -68,6 +99,11 @@ def build_explanation():
 
 
 def build_tabs():
+    """
+    This is one of the main builder functions where two tabs are created for the two pages of the web application.
+    within dcc.Tab() we run the builder functions as children.
+    :return: dash.html object.
+    """
     return html.Div(
         id='tabs',
         className='row container scalable',
@@ -145,6 +181,15 @@ def build_tabs():
 
 
 def build_mapbox_info_display(clickData):
+    """
+    This builds the right-hand side of the mapbox tab. Receiving data from the graph and processing the user click to
+    display the desired data.
+
+    :param clickData: Point data from mapbox scatter
+    :return: dash.html object
+    """
+
+    # For the first loading of the tab we set all the cells to be empty.
     name = ''
     price = ''
     service_fee = ''
@@ -153,6 +198,7 @@ def build_mapbox_info_display(clickData):
     min_nights = ''
     general_info = ''
 
+    # If a user click is detected:
     if clickData is not None:
         df_display = data[data['name'] == clickData['points'][0]['hovertext']]
         df_display = (df_display.reset_index()).iloc[0]
@@ -162,7 +208,7 @@ def build_mapbox_info_display(clickData):
         neighbourhood = df_display['neighbourhood']
         room_type = df_display['room_type']
         min_nights = df_display['minimum_nights']
-        general_info = 'PENIS!!!'
+        general_info = 'PENIS!!!'  # TODO: What is general info? What do we replace it with.
 
     return html.Div(
         className="row",
@@ -258,11 +304,19 @@ def build_mapbox_info_display(clickData):
 
 
 def build_mapbox_controls(neighbourhood_name):
+    """
+    This builds the top part of the mapbox tab with three dropdowns and two sliders
+    :param neighbourhood_name: One of the neighbourhood names from the csv file
+    :return: dash.html object
+    """
     # Define the basic variables
+    # Dropdown variables:
     neighbourhood_group = config.get_neighbourhood_groups()
     neighbourhood = config.get_neighbourhood()
-    min_price, max_price = config.get_price_min_max()
     instant_bookability = config.get_inst_bookable()
+
+    # Slider variables
+    min_price, max_price = config.get_price_min_max()
     min_service_fee, max_service_fee = config.get_service_fee()
 
     return html.Div(
@@ -299,7 +353,6 @@ def build_mapbox_controls(neighbourhood_name):
                                   children=[
                                       html.Label('Price Range', style={'margin-left': '20px'}),
                                       dcc.RangeSlider(
-                                          # TODO: ADD CLASSNAME HERE AND IN CSS
                                           min=min_price,
                                           max=max_price,
                                           value=[min_price, max_price],
@@ -348,6 +401,10 @@ def build_mapbox_controls(neighbourhood_name):
 
 
 def build_choropleth_tabs():
+    """
+    This builds the page for choropleth, barchart and their controls.
+    :return: dash.html object
+    """
     return html.Div(
         id="choropleth-content",
         className="row",
@@ -369,7 +426,6 @@ def build_choropleth_tabs():
                         type='graph',
                         children=[map_choropleth]
                     )
-                    # map_choropleth
                 ]
             ),
 
@@ -392,19 +448,25 @@ def build_choropleth_tabs():
                         children=[map_barchart]
 
                     ),
-                    # map_barchart
                 ]
             )
         ]
     )
 
 
+# ------------------------------------------------------------------------------------------------------#
+# ---------------------- Creating the global variables and importing data ----------------------------- #
+# ------------------------------------------------------------------------------------------------------#
 data = pd.read_csv("Data/airbnb_open_data_clean.csv", low_memory=False)
 
 map_boxplot = Mapboxplot("boxplot1", data)
 map_choropleth = Choropleth('choropleth', data)
 map_barchart = Barchart("barchart1", data)
 
+
+# ------------------------------------------------------------------------------------------------------#
+# ------------------------------ Building the main app layout ----------------------------------------- #
+# ------------------------------------------------------------------------------------------------------#
 app.layout = html.Div(
     children=[
         build_banner(),
@@ -418,6 +480,9 @@ app.layout = html.Div(
 )
 
 
+# ------------------------------------------------------------------------------------------------------#
+# ----------------------- Setting the callback functions for interactions ----------------------------- #
+# ------------------------------------------------------------------------------------------------------#
 @app.callback(Output('markdown', 'style'),
               Input('whats-this-button', 'n_clicks'),
               Input('markdown-close', 'n_clicks'))
@@ -474,7 +539,6 @@ def update_data_display(clickData):
         Input('app-tabs', 'value')]
 )
 def update_map_barchart(clickData, dropdown_choice, startup):
-
     if clickData is None:
         return map_barchart.update(None, None)
 
@@ -483,6 +547,4 @@ def update_map_barchart(clickData, dropdown_choice, startup):
         return map_barchart.update(name, dropdown_choice)
 
 
-app.run_server()
-
-# asd
+app.run_server(debug=False)
